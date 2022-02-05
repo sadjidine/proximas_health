11ccce569765f740a88d4aece16d406825c3ce08
Sadjidine <salaisse@gmail.com>
2022-02-05 21:09:29 +0000

    revision import res_partner

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 
+from . import res_partner
 from . import proximas_assure
 from . import proximas_reseau_soins
 from . import proximas_parametres
@@ -12,6 +13,5 @@ from . import proximas_police
 from . import proximas_prise_charge
 from . import proximas_facture
 from . import proximas_reporting
-from . import res_partner
 # from . import res_partner
 # from . import proximas_dashboard
